6a75396c403f9658159f0d79a5470501fc787ab6
unknown <nellysherova@mail.ru>
2021-03-27 08:10:57 +0300

    Начало работы

---
+++ MVCHomeWork/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MVCHomeWork.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
